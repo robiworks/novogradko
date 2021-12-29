a5ef07ca500dd607ad8c677183589073c9ac2dc8
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-29 20:05:48 +0100

    Update container ports

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Novogradko RESTful API is running on port ${PORT}.`);
