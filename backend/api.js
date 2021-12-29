4b1500a1d0c86b301ef200ea4b67cea47043d671
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-29 22:28:31 +0100

    Update backend API return values

--- backend/api.js
+++ backend/api.js
@@ -6,19 +6,19 @@ const Novogradko = require("./controllers/Novogradko");
 // Get all buildings
 router.get('/all', async (req, res) => {
     let all = await new Novogradko().getAll();
-    res.status(200).send(JSON.stringify(all));
+    res.status(200).json(all);
 });
 
 // Get apartments
 router.get('/apartments', async (req, res) => {
     let apartments = await new Novogradko().getApartments();
-    res.status(200).send(JSON.stringify(apartments));
+    res.status(200).json(all);
 });
 
 // Get houses
 router.get('/houses', async (req, res) => {
     let houses = await new Novogradko().getHouses();
-    res.status(200).send(JSON.stringify(houses));
+    res.status(200).json(all);
 });
 
 module.exports = router;
