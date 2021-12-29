6d46c0561d011974d998dc5878407d7dbec56162
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-29 22:29:23 +0100

    Update frontend display JavaScript

--- frontend/map.js
+++ frontend/map.js
@@ -11,11 +11,24 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 OpenStreetMap_Mapnik.addTo(map);
 L.control.scale().addTo(map);
 
-var mapElement = document.getElementById('map');
-var navbarElement = document.getElementById('navbar');
-
 function setMapHeight() {
+    let mapElement = document.getElementById('map');
+    let navbarElement = document.getElementById('navbar');
     mapElement.style.height = window.innerHeight - navbarElement.clientHeight + "px";
 }
 
-window.addEventListener('resize', setMapHeight());
+window.addEventListener('resize', setMapHeight);
+
+document.addEventListener('DOMContentLoaded', populateMarkers);
+document.addEventListener('DOMContentLoaded', setMapHeight);
+
+function populateMarkers() {
+    getBuildings().then(data => {
+        for (let i = 0; i < data.length; i++) {
+            let lat = parseFloat(data[i].lat);
+            let lon = parseFloat(data[i].lon);
+            let name = data[i].name;
+            L.marker([lat, lon]).bindPopup(name).addTo(map);
+        }
+    });
+}
