5beea1c30065a4865dc96941bc59882f3b43d7bd
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-29 16:39:32 +0100

    Add database container

---
+++ database/init.sql
@@ -0,0 +1,18 @@
+GRANT ALL PRIVILEGES ON DATABASE novogradko TO user;
+
+CREATE TABLE IF NOT EXISTS buildings (
+    id      SERIAL PRIMARY KEY NOT NULL,
+    name    TEXT NOT NULL,
+    lon     DOUBLE PRECISION,
+    lat     DOUBLE PRECISION,
+    type    SMALLINT,
+    website TEXT
+);
+
+INSERT INTO buildings (name, lon, lat, type, website)
+VALUES
+    ('Regentov kvart', 0.0, 0.0, 0, 'https://www.ljubljananepremicnine.si/novogradnja/regentov-kvart'),
+    ('Barjanka', 0.0, 0.0, 0, 'https://www.ljubljananepremicnine.si/novogradnja/barjanka'),
+    ('Lagom Črnuški bajer', 0.0, 0.0, 0, 'https://www.crnuski-bajer.si/'),
+    ('Palais & Villa Schellenburg', 0.0, 0.0, 0, 'https://www.schellenburg-living.com/sl/celota.html'),
+    ('Vila Toscanini', 0.0, 0.0, 0, 'https://vilatoscanini.si/');
