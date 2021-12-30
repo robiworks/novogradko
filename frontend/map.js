bcbf3b30c9c330250d97aa1c2fbb391c28ee2235
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-30 14:13:46 +0100

    Add custom map marker icons

--- frontend/map.js
+++ frontend/map.js
@@ -22,13 +22,31 @@ window.addEventListener('resize', setMapHeight);
 document.addEventListener('DOMContentLoaded', populateMarkers);
 document.addEventListener('DOMContentLoaded', setMapHeight);
 
+// Icons made by juicy_fish from www.flaticon.com
+var MapIcon = L.Icon.extend({
+    options: {
+        iconSize: [48, 48],
+        iconAnchor: [24, 48],
+        popupAnchor: [0, -48],
+        tooltipAnchor: [0, -48]
+    }
+});
+
+var houseIcon = new MapIcon({iconUrl: './icons/home.png'});
+var apartmentIcon = new MapIcon({iconUrl: './icons/offices.png'});
+
 function populateMarkers() {
     getBuildings().then(data => {
         for (let i = 0; i < data.length; i++) {
             let lat = parseFloat(data[i].lat);
             let lon = parseFloat(data[i].lon);
             let name = data[i].name;
-            L.marker([lat, lon]).bindPopup(name).addTo(map);
+            let type = parseInt(data[i].type);
+            if (type === 0) {
+                L.marker([lat, lon], {icon: apartmentIcon}).bindPopup(name).addTo(map);
+            } else {
+                L.marker([lat, lon], {icon: houseIcon}).bindPopup(name).addTo(map);
+            }
         }
     });
 }
